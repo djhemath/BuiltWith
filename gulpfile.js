1ed08eb2101dd9c2bdf2c6ad421f767be5740e9a
DJ Hemath <djhemath@gmail.com>
2022-10-23 19:19:17 +0530

    config: updated build configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const minify = require('gulp-minify');
+const gulp = require('gulp');
+const browserify = require('gulp-browserify');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const Babelify = require('babelify');
 
-const tsProject = ts.createProject('tsconfig.json');
-
-gulp.task("default", function () {
-    return tsProject.src()
-            .pipe(tsProject())
-            .js
-            .pipe(minify())
-            .pipe(gulp.dest('build'));
+gulp.task('clean', function () {
+    return gulp
+        .src([
+            './tmp/',
+            './build/'
+        ], { read: false, allowEmpty: true })
+        .pipe(clean());
 });
+
+gulp.task('transpile-ts', gulp.series('clean', function () {
+    const tsproject = ts.createProject('./tsconfig.json');
+    return tsproject
+        .src()
+        .pipe(tsproject()).js
+        .pipe(gulp.dest('./tmp/js'));
+}));
+
+gulp.task('min-js', gulp.series('transpile-ts', function () {
+    return gulp
+        .src('./tmp/js/built-with.component.js')
+        .pipe(browserify({transform: [Babelify.configure({presets: ['@babel/preset-env']})]}))
+        .pipe(concat('built-with.component.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build'))
+}));
+
+gulp.task('default', gulp.series('min-js'));
